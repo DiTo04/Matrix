a3ef5db460b89410fa8940322dd91246a256b5e9
David Tennander <david.tennander@gmail.com>
2017-07-05 01:32:16 +0200

    Cleaning up parts.

--- src/Numeric/Matrix.hs
+++ src/Numeric/Matrix.hs
@@ -143,13 +143,11 @@ class (Num e, Eq e) => MatrixElement e where
 
   det :: Matrix e -> e 
   det mat@(Matrix (n,m) _) 
-    | n == m && n>2    = P.sum $ zipWith (\c i -> c * (det $ removeRowCol 1 i mat)) coef intList
+    | n == m && n>2    = P.sum $ zipWith (\c i -> c * (det $ removeRowCol 1 i mat)) coef [1..]
     | n == m && n == 2 = (mat `at` (1,1)) * (mat `at` (2,2)) - ((mat `at` (1,2)) * (mat `at` (2,1)))
     | otherwise        = 0
     where
       coef = zipWith (*) (row 1 mat) (cycle [1,-1])
-      intList = 1 : P.map (+1) intList :: [Int]
-
 
   removeRowCol :: Int -> Int -> Matrix e -> Matrix e
   removeRowCol i j mat@(Matrix (n,m) _) = Matrix (n-1,m-1) $ chunksOf (n-1) elements 
